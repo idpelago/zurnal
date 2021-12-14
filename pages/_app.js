6266bcea57f94b329461caf1444f47ae2a31d68f
woltbit <woltbit@gmail.com>
2021-12-14 13:24:34 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -25,9 +25,7 @@ const queryClient = new QueryClient(getQueryClientConfig());
 
 const App = ({ Component, pageProps }) => {
   const children = <Component {...pageProps} />;
-  const withLayout = Component.getLayout?.(children)?.(pageProps)
-    ? Component.getLayout?.(children)?.(pageProps)
-    : children;
+  const withLayout = Component.getLayout?.(children)?.(pageProps) ?? children;
 
   return (
     <>
