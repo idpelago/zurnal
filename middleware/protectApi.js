f3d363e1ca8de8ed368a00f87b69efaf3269fcbe
woltbit <woltbit@gmail.com>
2021-12-15 15:33:14 +0800

    update

--- middleware/protectApi.js
+++ middleware/protectApi.js
@@ -2,6 +2,7 @@ const SITE_URL = process.env.SITE_URL;
 
 const protectApi = (handler) => {
   return async (req, res) => {
+    console.log("req.headers.referer : ", req.headers.referer);
     if (new URL(req.headers.referer).origin !== SITE_URL) {
       return res.status(403).json({ success: false, message: `Forbidden` });
     }
