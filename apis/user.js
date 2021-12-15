d4cbd900f4398671a3ed2b553a5864fe0287870a
idpelago <idpelago@gmail.com>
2021-12-16 00:39:53 +0800

    update

--- apis/user.js
+++ apis/user.js
@@ -1,6 +1,6 @@
 import { api } from "./api";
 
-export const getTag = async ({ username, page = null }) => {
+export const getUser = async ({ username, page = null }) => {
     let userQuery = {};
 
     if (page !== null) userQuery.page = page;
