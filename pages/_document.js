a7a3d64d12aa22499a90fb5a36d7ca19c90fa64f
woltbit <woltbit@gmail.com>
2021-12-17 11:51:46 +0800

    update

--- pages/_document.js
+++ pages/_document.js
@@ -109,7 +109,6 @@ class Document extends NextDocument {
             sizes="16x16"
             href="https://www.zurnal.co/images/favicon-16x16.png"
           />
-          <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta
             name="msapplication-TileImage"
