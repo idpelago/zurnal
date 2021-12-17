2e696e7f840caea55d6bf2d0ccfd7eee5950139b
woltbit <woltbit@gmail.com>
2021-12-17 18:08:47 +0800

    scroll smooth

--- pages/_app.js
+++ pages/_app.js
@@ -42,7 +42,7 @@ const App = ({ Component, pageProps }) => {
   useEffect(() => {
     setMode(window.innerWidth < minWidth ? "mobile" : "desktop");
 
-    const handleChange = () => window.scrollTo(0, 0);
+    const handleChange = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
     router.events.on("routeChangeComplete", handleChange);
 
