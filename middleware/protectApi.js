a7cd573e471a710c15af72f2c3a80cd32abd57e3
idpelago <idpelago@gmail.com>
2021-12-19 12:46:48 +0800

    update

--- middleware/protectApi.js
+++ middleware/protectApi.js
@@ -1,10 +1,11 @@
 const SITE_URL = process.env.SITE_URL;
+const ENV_APP = process.env.ENV_APP;
 
 const protectApi = (handler) => {
   return async (req, res) => {
-    if (
-      !req.headers.referer ||
-      new URL(req.headers.referer).origin !== SITE_URL
+    if ((ENV_APP == 'production') &&
+      (!req.headers.referer ||
+        new URL(req.headers.referer).origin !== SITE_URL)
     ) {
       return res.status(403).json({ success: false, message: `Forbidden` });
     }
