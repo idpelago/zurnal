f018063031e5fbf8cc70f404903f01028607d9af
idpelago <idpelago@gmail.com>
2021-12-23 11:11:45 +0800

    update

--- pages/post/[postSlugId]/[postSlugTitle]/index.js
+++ pages/post/[postSlugId]/[postSlugTitle]/index.js
@@ -1,4 +1,6 @@
-import { useEffect } from "react";
+import Script from "next/script";
+
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import Layout from "../../../../Components/Layout";
@@ -9,6 +11,7 @@ import NewsFeedRightSection from "../../../../Sections/NewsFeed/RightSide";
 
 import { getPost } from "../../../../apis";
 import { processSSR } from "../../../../utils/helpers";
+import config from "../../../../utils/config";
 
 const PostContent = (props) => {
   const router = useRouter();
@@ -19,41 +22,44 @@ const PostContent = (props) => {
     ...props,
   };
 
+  const { minWidth } = config;
+  const [mode, setMode] = useState();
+
   useEffect(() => {
-    function loadAPI() {
-      var js = document.createElement("script");
+    setMode(window.innerWidth < minWidth ? "mobile" : "desktop");
 
-      js.src =
-        "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=396954390897339&version=v2.0";
-      document.body.appendChild(js);
-    }
-
-    setTimeout(() => loadAPI(), 1000);
+    setTimeout(() => window.FB.XFBML.parse(), 1000)
   }, [postSlugId, postSlugTitle]);
 
   return (
-    <section className="utf_block_wrapper">
-      <div className="container">
-        <div className="row">
-          <PostContentSection {...params} />
-          <NewsFeedRightSection />
-        </div>
+    <>
+      <Script src="//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=396954390897339&version=v2.0"
+        strategy="afterInteractive" />
 
-        <div className="row">
-          <div className="col-lg-8 col-md-12">
-            <div id="fb-root"></div>
-            <div
-              id="comments"
-              className="fb-comments"
-              data-href={`https://www.zurnal.co/post/${postSlugId}/${postSlugTitle}`}
-              data-colorscheme="light"
-              data-width="100%"
-              data-numposts="5"
-            ></div>
+      <section className="utf_block_wrapper">
+        <div className="container">
+          <div className="row">
+            <PostContentSection {...params} />
+
+            {mode == 'desktop' ? <NewsFeedRightSection /> : ''}
+          </div>
+
+          <div className="row">
+            <div className="col-lg-8 col-md-12">
+              <div id="fb-root"></div>
+              <div
+                id="comments"
+                className="fb-comments"
+                data-href={`https://www.zurnal.co/post/${postSlugId}/${postSlugTitle}`}
+                data-colorscheme="light"
+                data-width="100%"
+                data-numposts="5"
+              ></div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
