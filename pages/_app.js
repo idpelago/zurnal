cabaa90a1cd8982015db0f0c491c606a9bc2832d
woltbit <woltbit@gmail.com>
2022-02-12 12:26:20 +0800

    add blacklisted ip

--- pages/_app.js
+++ pages/_app.js
@@ -121,6 +121,7 @@ App.getInitialProps = async (context) => {
     "59.124.110.2", // Jenni's office
     "61.222.146.133", // kkbox H 棟
     "124.218.17.145", // Home ip
+    "1.200.251.192", // Juan's phone ip
   ];
 
   const currentIp =
