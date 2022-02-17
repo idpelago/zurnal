1ebb5eea2167b77f639aa5e3f92bb82cd6432781
woltbit <woltbit@gmail.com>
2022-02-17 14:27:22 +0800

    update

--- Sections/Footer/index.js
+++ Sections/Footer/index.js
@@ -29,27 +29,29 @@ const FooterSection = () => {
           <div className="vce-wrap-left">
             <p>
               <span id="copyRightFooter">
-                Copyright © 2017 <a href="#">Zurnal</a> All Rights Reserved.
-                </span>
+                <FooterTextFirst />
+                <FooterLink />
+                <FooterTextSecond />
+              </span>
             </p>
           </div>
 
           <div className="vce-wrap-right">
             <ul id="vce_footer_menu" className="bottom-nav-menu">
               <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-7283">
-                <a href="#about-us/">About Us</a>
+                <a href="#">About Us</a>
               </li>
               <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-7294">
-                <a href="#term-of-services/">Term of Services</a>
+                <a href="#">Term of Services</a>
               </li>
               <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-7282">
-                <a href="#privacy-policy/">Privacy Policy</a>
+                <a href="#">Privacy Policy</a>
               </li>
               <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-7290">
-                <a href="#end-user-license-agreement/">EULA</a>
+                <a href="#">EULA</a>
               </li>
               <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-7291">
-                <a href="#contact-us/">Contact Us</a>
+                <a href="#">Contact Us</a>
               </li>
             </ul>
           </div>
