f0b3232e0edeac952447ecb6932b242cdcf65f1c
woltbit <woltbit@gmail.com>
2022-02-17 14:31:16 +0800

    update protectApi middleware

--- middleware/protectApi.js
+++ middleware/protectApi.js
@@ -1,11 +1,10 @@
 const SITE_URL = process.env.SITE_URL;
-const ENV_APP = process.env.ENV_APP;
 
 const protectApi = (handler) => {
   return async (req, res) => {
     if (
-      ENV_APP == "production" &&
-      (!req.headers.referer || new URL(req.headers.referer).origin !== SITE_URL)
+      !req.headers.referer ||
+      new URL(req.headers.referer).origin !== SITE_URL
     ) {
       return res.status(403).json({ success: false, message: `Forbidden` });
     }
